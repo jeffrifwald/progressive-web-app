eac4bc5febbe852241ce2110712f9ba5f5bd4288
Jeff McRiffey <jmcriffey@users.noreply.github.com>
2019-06-12 18:37:06 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,16 @@
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js') // Load WorkBox
 
-
 console.log('Hello Service Worker!');
+
+workbox.routing.registerRoute( // Cache a route
+  /^https:\/\/fonts\.googleapis\.com/, // RegExp checks fetch requests 
+  new workbox.strategies.StaleWhileRevalidate({ // Pick a caching strategy
+    cacheName: 'google-fonts-stylesheets',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 10, // Only allow 10 responses in the cache
+        maxAgeSeconds: 60 * 60 * 24 * 14 // Cache for 14 days
+      })
+    ]
+  })
+);
