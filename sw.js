fea52b6aa3c853308c64a217f5ab596f37faef54
Jeff McRiffey <jmcriffey@users.noreply.github.com>
2019-06-12 18:38:35 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,9 +5,9 @@ console.log('Hello Service Worker!');
 workbox.routing.registerRoute( // Cache a route
   /^https:\/\/fonts\.googleapis\.com/, // RegExp checks fetch requests 
   new workbox.strategies.StaleWhileRevalidate({ // Pick a caching strategy
-    cacheName: 'google-fonts-stylesheets',
+    cacheName: 'google-fonts-stylesheets', // Give a unique name to the cache
     plugins: [
-      new workbox.expiration.Plugin({
+      new workbox.expiration.Plugin({ // Manage cache expiration
         maxEntries: 10, // Only allow 10 responses in the cache
         maxAgeSeconds: 60 * 60 * 24 * 14 // Cache for 14 days
       })
